3d0bfe1ccd7153c98ecbf2b66270569062a55dfb
wasabina67 <wasabina67@gmail.com>
2024-10-13 00:13:52 +0900

    Update notify.py

--- notify.py
+++ notify.py
@@ -1,1 +1,19 @@
+import os
 
+import requests
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+
+def main():
+    webhook_url = os.getenv("DISCORD_WEBHOOK_URL")
+    payload = {
+        "content": "content",
+    }
+    resp = requests.post(webhook_url, json=payload)
+    print(resp.status_code)
+
+
+if __name__ == "__main__":
+    main()
